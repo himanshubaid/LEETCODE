9295178921d09bb8a29a8c63f856ec770ea95158
Himanshu Baid <108008314+himanshubaid@users.noreply.github.com>
2023-06-02 11:49:13 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -17,12 +17,49 @@ class Solution
 	    }
 	    st.push(node);
 	}
+	vector<int> bfs(int V, vector<int> adj[])
+	{
+	    vector<int> ans;
+	    
+	    vector<int>indegree(V,0);
+	    
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto it:adj[i])
+	        {
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int>q;
+	    for(int i=0;i<V;i++)
+	    {
+	        if(indegree[i]==0)
+	        q.push(i);
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        int node=q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        
+	        for(auto it:adj[node])
+	        {
+	            indegree[it]--;
+	            if(indegree[it]==0)
+	            q.push(it);
+	        }
+	    }
+	    
+	    return ans;
+	}
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    stack<int>st;
 	    vector<int>vis(V,0);
 	    
-	    for(int i=0;i<V;i++)
+	   /* for(int i=0;i<V;i++)
 	    {
 	        if(!vis[i])
 	        {
@@ -30,15 +67,15 @@ class Solution
 	        }
 	    }
 	    
-	    vector<int>ans;
+	    
 	    while(!st.empty())
 	    {
 	        int top=st.top();
 	        ans.push_back(top);
 	        st.pop();
-	    }
+	    }*/
 	    
-	    return ans;
+	    return bfs(V,adj);
 	}
 };
 
