a73f8f346efdd8e38b8ff44c60ea367c85c5994b
Himanshu Baid <108008314+himanshubaid@users.noreply.github.com>
2023-06-16 14:54:42 +0530

    Added solution - LeetHub

---
+++ Implement stack using array - GFG/implement-stack-using-array.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+
+
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyStack *sq = new MyStack();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//Function to push an integer into the stack.
+
+/*
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+*/
+
+void MyStack :: push(int x)
+{   
+    if(top<-1)
+    top=-1;
+    arr[top+1]=x;
+    top=top+1;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{   
+    top--;
+    int no=-1;
+    if(top+1>=0)
+    no=arr[top+1];
+    return no;
+    
+}
